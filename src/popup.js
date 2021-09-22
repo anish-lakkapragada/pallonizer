94a23ca4cbe4a80c28d5ddcdc2900ec25c5e163c
anish-lkakapragada <anish.lakkapragada@gmail.com>
2021-09-21 21:21:34 -0700

    update

--- src/popup.js
+++ src/popup.js
@@ -67,19 +67,6 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 
 $(() => {
     $("#input-check").click(() => {
-
-        /*chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {}, function (response) {
-                update(response.ALLOWED);
-                
-                if (response.ALLOWED == false) {
-                    console.log("reloading"); 
-                    chrome.tabs.reload(tabs[0].id);
-                    console.log("reloaded"); 
-                }
-            });
-        });*/ 
-
         checked = !checked;
         update(checked);
         
